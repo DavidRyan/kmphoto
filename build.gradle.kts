87a58718c0baa74aefcc862f0e0239f22847ba49
David Ryan <dryan@mbp.local>
2026-03-02 20:23:32 -0600

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,4 @@ plugins {
     kotlin("multiplatform") version "1.9.24" apply false
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.compose") version "1.6.11" apply false
-    id("maven-publish") apply false
 }
